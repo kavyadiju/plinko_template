1d586dfc79705247ed338aad0f79a3610c3a150a
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2021-06-10 18:32:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,8 +66,7 @@ function draw() {
      
    }
    if(frameCount%60===0){
-     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
-     score++;
+   
    }
  
   for (var j = 0; j < particles.length; j++) {
